1fd9e1fef7b578b8cec65164de018764a53a7d15
Emanuel Pontoni <pontoni.emanuel@gmail.com>
2021-09-08 17:21:02 -0300

    notes with json-server

--- part2/notes/src/index.js
+++ part2/notes/src/index.js
@@ -3,30 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const notes = [
-    {
-        id: 1,
-        content: 'Text note 1',
-        date: '',
-        important: Math.random() < 0.5
-    },
-    {
-        id: 2,
-        content: 'Text note 2',
-        date: '',
-        important: Math.random() < 0.5
-    },
-    {
-        id: 3,
-        content: 'Text note 3',
-        date: '',
-        important: Math.random() < 0.5
-    }
-]
-
 ReactDOM.render(
-  <React.StrictMode>
-    <App notes={notes} />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
